06e0d6f70319421a13d58c4213b6f65bf2dcdaca
marceltanuri <marcel.tanuri@gmail.com>
2022-10-02 23:43:40 +0100

    put in UF order

--- index.js
+++ index.js
@@ -3,34 +3,35 @@ import fetch from 'node-fetch';
 
 class Resultado {
 
-    
-    static ufs = ["ac", "al", "ap", "am", "ba", "ce", "df", "es", "go", "ma", "mt", "ms", "mg", "pa", "pb", "pr", "pe", "pi", "rj", "rn", "rs", "ro", "rr", "sc", "sp", "se", "to"]
 
-    static getResults() {
-        this.ufs.forEach(uf => {
 
-            fetch(`https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/${uf}/${uf}-c0001-e000544-r.json`).then(res => {
-                res.json().then(json => {
-                    try {
-                        const primeiroLugar = json.cand.reduce(function (prev, current) {
-                            return (prev.seq < current.seq) ? prev : current
-                        })
-                        console.log(uf.toUpperCase())
-                        console.log(primeiroLugar.nm)
-                        console.log(primeiroLugar.pvap + "%")
-                        console.log(primeiroLugar.vap)
-                        console.log("=========================================")
-                    } catch (error) {
+    static async getResults() {
 
+
+        const ufs = ["ac", "al", "ap", "am", "ba", "ce", "df", "es", "go", "ma", "mt", "ms", "mg", "pa", "pb", "pr", "pe", "pi", "rj", "rn", "rs", "ro", "rr", "sc", "sp", "se", "to"]
+        const urls = ufs.map(uf => `https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/${uf}/${uf}-c0001-e000544-r.json`);
+        const response = await Promise.all(urls.map(url => fetch(url)))
+        await Promise.all(response.map(res => {
+            try {
+                res.json().then(data => {
+                    const primeiroLugar = data.cand.reduce(function (prev, current) {
+                        return (prev.seq < current.seq) ? prev : current
+                    })
+
+                    let result = {
+                        UF: data.cdabr,
+                        Apuração: data.pst + "%",
+                        Candidato: primeiroLugar.nm,
+                        "% dos votos": primeiroLugar.pvap + "%",
+                        "Total de votos": primeiroLugar.vap
                     }
+                    console.log(result)
+                    console.log("=========================================")
                 })
-            })
-
-
-        })
-
+            } catch (error) {
+            }
+        }))
     }
-
 }
 
 Resultado.getResults()
